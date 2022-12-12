9fc70e39b800329fd9c332ea8292bf87396ba0bb
Filip Riha <riha.fina@gmail.com>
2022-12-12 18:16:35 +0100

    Add dummy Main

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,22 @@
 module Main (main) where
 
+import HaSOM.Parser.Happy
+import HaSOM.Lexer.Alex
+import System.Environment (getArgs)
+
 main :: IO ()
-main = print "Hello World"
+main = getArgs >>= mapM_ lexAndParse
+
+lexAndParse file = do
+  putStrLn $ "File: " ++ file
+
+  content <- readFile file
+  let tokens = alexScanTokens content
+  mapM_ print tokens
+
+  putStrLn ""
+
+  let ast = parse tokens
+  print ast
+
+  putStrLn "--------------"
