be81a137cd9a91e2ea761de161666e476cf0ddf9
Filip Riha <riha.fina@gmail.com>
2023-04-20 21:46:13 +0200

    Add CoreClasses, change Class super to GlobalIx

--- src/HaSOM/VM/Primitive/VMClass.hs
+++ src/HaSOM/VM/Primitive/VMClass.hs
@@ -1,13 +1,40 @@
-module HaSOM.VM.Primitive.VMClass(VMClass(..)) where
+module HaSOM.VM.Primitive.VMClass (VMClass (..), CoreClasses (..)) where
 
-import HaSOM.VM.Primitive.Ix ( ObjIx )
-import HaSOM.VM.Primitive.VMMethod ( VMMethods )
+import HaSOM.VM.Primitive.Ix (GlobalIx, ObjIx)
+import HaSOM.VM.Primitive.VMMethod (VMMethods)
 
 -- | Representation of SOM Class,
 -- parametrised by native function type
 data VMClass f = MkVMClass
   { fieldsCount :: Int,
-    superclass :: Maybe (VMClass f),
+    superclass :: Maybe GlobalIx,
     asObject :: ObjIx,
     methods :: VMMethods f
+  }
+
+data CoreClasses = MkCoreClasses
+  { -- meta objects
+    classClass :: GlobalIx,
+    metaclassClass :: GlobalIx,
+    objectClass :: GlobalIx,
+    -- system
+    systemClass :: GlobalIx,
+    -- invokables
+    methodClass :: GlobalIx,
+    primitiveClass :: GlobalIx,
+    -- primitives
+    booleanClass :: GlobalIx,
+    integerClass :: GlobalIx,
+    doubleClass :: GlobalIx,
+    stringClass :: GlobalIx,
+    symbolClass :: GlobalIx,
+    arrayClass :: GlobalIx,
+    -- nilClass :: GlobalIx,
+    -- trueClass :: GlobalIx,
+    -- falseClass :: GlobalIx,
+    -- block
+    blockClass :: GlobalIx,
+    block1Class :: GlobalIx,
+    block2Class :: GlobalIx,
+    block3Class :: GlobalIx
   }
