a4b619533115c11d773845b112765916575051ef
Filip Riha <riha.fina@gmail.com>
2023-04-20 21:44:23 +0200

    Change bytecode

--- src/HaSOM/VM/Primitive/Bytecode.hs
+++ src/HaSOM/VM/Primitive/Bytecode.hs
@@ -2,20 +2,21 @@
 module HaSOM.VM.Primitive.Bytecode(Bytecode(..), Code) where
 import HaSOM.VM.Primitive.VMArray (VMArray)
 import HaSOM.VM.Primitive.Ix
-import Data.Stack (StackIx)
 
 -- | Representation of SOM Bytecode
 data Bytecode
   = HALT
-  | PUSH_SYMBOL SymbolIx
-  | PUSH_LOCAL StackIx
+  | DUP
+  | POP
+  | PUSH_LITERAL LiteralIx
+  | PUSH_LOCAL LocalIx LocalIx
   | PUSH_FIELD FieldIx
   | PUSH_GLOBAL GlobalIx
-  | SET_LOCAL StackIx
+  | SET_LOCAL LocalIx LocalIx
   | SET_FIELD FieldIx
   | SET_GLOBAL GlobalIx
-  | CALL
-  | SUPER_CALL
+  | CALL LiteralIx
+  | SUPER_CALL LiteralIx
   | RETURN
   | NONLOCAL_RETURN
   deriving (Eq, Show)
