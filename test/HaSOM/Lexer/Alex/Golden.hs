6311a892eb36f946abd8cead21e80a6f73ebc661
Filip Riha <riha.fina@gmail.com>
2023-04-30 14:24:52 +0200

    Use tokens pretty print in Golden tests

--- test/HaSOM/Lexer/Alex/Golden.hs
+++ test/HaSOM/Lexer/Alex/Golden.hs
@@ -2,8 +2,8 @@ module HaSOM.Lexer.Alex.Golden(golden) where
 
 import HaSOM.Lexer.Alex
 import WithSources (SourceGolden)
-import Data.Text.Utility (showT)
 import qualified Data.Text as T
+import HaSOM.Lexer.Token (tokenToText)
 
 golden :: SourceGolden
-golden = ("Alex", T.unlines . map (\(PosToken _ tk) -> showT tk) . alexScanTokens)
+golden = ("Alex", T.unlines . map (\(PosToken _ tk) -> tokenToText tk) . alexScanTokens)
