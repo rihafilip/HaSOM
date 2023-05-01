57a092dd18d05fd0882a7cf50856c52956449ccd
Filip Riha <riha.fina@gmail.com>
2023-05-01 18:57:13 +0200

    Add PrettyPrint documentation

--- src/Data/PrettyPrint.hs
+++ src/Data/PrettyPrint.hs
@@ -3,7 +3,23 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
 
-module Data.PrettyPrint where
+-- | A pretty printing effect
+module Data.PrettyPrint
+  ( -- * Run formating
+    PrettyPrintEff,
+    runPrettyPrint,
+
+    -- * Basic interface
+    indented,
+    addLine,
+
+    -- * Formating helpers
+    (.:),
+    (<#>),
+    formatList,
+    quote,
+  )
+where
 
 import Control.Eff (Eff, type (<::))
 import Control.Eff.Reader.Strict
@@ -12,25 +28,30 @@ import Data.Text (Text)
 import qualified Data.Text as T
 import Data.Text.Utility
 
+-- | The pretty printing effect
 type PrettyPrintEff r = [Reader Int, Writer Text] <:: r
 
+-- | Run pretty printing
 runPrettyPrint :: Eff (Reader Int : Writer Text : r) a -> Eff r Text
 runPrettyPrint =
   (T.unlines <$>)
     . (execListWriter @Text)
     . runReader (0 :: Int)
 
+-- | Add one level of indentation
 indented :: PrettyPrintEff r => Eff r () -> Eff r ()
 indented f = do
   (currIndent :: Int) <- ask
   local (const $ currIndent + 1) f
 
+-- | Output a line
 addLine :: PrettyPrintEff r => Text -> Eff r ()
 addLine str = do
   (indentSize :: Int) <- ask
   let indentatiton = T.replicate indentSize "  "
   tell $ indentatiton <+ str
 
+-- | "a" .: "b" prints as "a: b"
 (.:) :: PrettyPrintEff r => Text -> Text -> Eff r ()
 name .: value = addLine (name <+ ": " <+ value)
 
@@ -38,8 +59,10 @@ name .: value = addLine (name <+ ": " <+ value)
 (<#>) :: PrettyPrintEff r => Text -> Text -> Eff r ()
 name <#> field = addLine $ "(" <+ name <+ " " <+ field <+ ")"
 
+-- | Format a list
 formatList :: [Text] -> Text
 formatList list = "[ " <+ T.unwords list <+ " ]"
 
+-- | Quote given text
 quote :: Text -> Text
 quote = ("\"" <+) . (<+ "\"")
